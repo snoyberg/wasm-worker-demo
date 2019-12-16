ba6a5e3fee694c0a293a141d2fc07f3d6775586b
Michael Snoyman <michael@snoyman.com>
2019-12-16 14:01:46 +0200

    Marshaling values, askama templates, routing

--- worker/worker.js
+++ worker/worker.js
@@ -7,8 +7,29 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-    const { greet } = wasm_bindgen;
+    const { respond_wrapper } = wasm_bindgen;
     await wasm_bindgen(wasm)
-    const greeting = greet()
-    return new Response(greeting, {status: 200})
+
+    var body;
+    if (request.body) {
+        body = await request.text();
+    } else {
+        body = "";
+    }
+
+    var headers = {};
+    for(var key of request.headers.keys()) {
+        headers[key] = request.headers.get(key);
+    }
+
+    const response = await respond_wrapper({
+        method: request.method,
+        headers: headers,
+        url: request.url,
+        body: body,
+    })
+    return new Response(response.body, {
+        status: response.status,
+        headers: response.headers,
+    })
 }
